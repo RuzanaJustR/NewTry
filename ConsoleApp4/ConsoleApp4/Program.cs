181f1cfd3263bf5f7423dbb9e4fdd642ec3ed276
Пользователь <Ruzana-Klop@mail.ru>
2023-09-27 15:31:19 +0500

    my first commit

---
+++ ConsoleApp4/ConsoleApp4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.WriteLine("I am here...<Допишите что-нибудь>");
+        }
+    }
+}
